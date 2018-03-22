1f231e00dac8de000b42e21b0adfa914e5a4a202
PavlenkoB <godexxxx@gmail.com>
2018-03-22 16:55:40 +0200

    add test for linkedlist

---
+++ other/src/main/java/ua.ho.godex/TestLinkedListVsArrayList.java
@@ -0,0 +1,33 @@
+package ua.ho.godex;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Creator: Pavlenko Bohdan
+ * Date: 22.03.2018
+ * Project: java-learn
+ */
+public class TestLinkedListVsArrayList {
+    public static void main(String[] args) {
+        ArrayList testArrayList = new ArrayList();
+        LinkedList testLinkedList = new LinkedList();
+//        testAddToList(testLinkedList);
+        testAddToList(testArrayList);
+    }
+
+    public static void testAddToList(List list) {
+        System.out.println();
+        System.out.println(list.getClass().getSimpleName());
+        Integer elementsCounter = 10000;
+        Integer element = new Integer(1);
+        long startTime = System.currentTimeMillis();
+        do {
+            list.add(list.size() / 2, element);
+            element++;
+        } while (elementsCounter-- > 0);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time=" + (endTime - startTime));
+    }
+}
