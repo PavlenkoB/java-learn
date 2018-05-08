31e25543c24208a23af1ae8d87342617d6f6f718
PablenkoB <pavlenkobohdan@gmail.com>
2018-05-08 17:40:46 +0300

    end Spring - Bean Definition Inheritance https://www.tutorialspoint.com/spring/spring_bean_definition_inheritance.htm

--- spring/src/main/java/ua/ho/godex/learnspring/HelloWorld.java
+++ spring/src/main/java/ua/ho/godex/learnspring/HelloWorld.java
@@ -1,32 +1,20 @@
 package ua.ho.godex.learnspring;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.context.annotation.Bean;
+public class HelloWorld {
 
-import javax.annotation.PreDestroy;
+    private String message1;
+    private String message2;
 
-public class HelloWorld implements DisposableBean,InitializingBean {
-    private String message;
-
-    public void setMessage(String message) {
-        this.message = message;
+    public void setMessage1(String message){
+        this.message1 = message;
     }
-
-    public void getMessage() {
-        System.out.println("Your Message : " + message);
+    public void setMessage2(String message){
+        this.message2 = message;
     }
-
-    public void init() {
-        System.out.println("Bean is going through init.");
+    public void getMessage1(){
+        System.out.println("World Message1 : " + message1);
     }
-
-    public void destroy() {
-        System.out.println("Bean will destroy now.");
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("afterPropertiesSet");
+    public void getMessage2() {
+        System.out.println("World Message2 : " + message2);
     }
 }
