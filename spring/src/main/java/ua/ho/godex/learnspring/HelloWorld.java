a80e7210447d3cea39b7d9c5b6c4f546bc48b158
PablenkoB <pavlenkobohdan@gmail.com>
2018-05-08 17:27:39 +0300

    spring before "Bean Definition Inheritance"

--- spring/src/main/java/ua/ho/godex/learnspring/HelloWorld.java
+++ spring/src/main/java/ua/ho/godex/learnspring/HelloWorld.java
@@ -1,12 +1,32 @@
 package ua.ho.godex.learnspring;
 
-public class HelloWorld {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PreDestroy;
+
+public class HelloWorld implements DisposableBean,InitializingBean {
     private String message;
 
-    public void setMessage(String message){
-        this.message  = message;
+    public void setMessage(String message) {
+        this.message = message;
     }
-    public void getMessage(){
+
+    public void getMessage() {
         System.out.println("Your Message : " + message);
+    }
+
+    public void init() {
+        System.out.println("Bean is going through init.");
+    }
+
+    public void destroy() {
+        System.out.println("Bean will destroy now.");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet");
     }
 }
