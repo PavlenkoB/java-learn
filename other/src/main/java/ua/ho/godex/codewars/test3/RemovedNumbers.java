9ed3c6c19160767be7e16b8827dfd380cb04d878
PablenkoB <pavlenkobohdan@gmail.com>
2018-05-10 17:45:17 +0300

    add some test

---
+++ other/src/main/java/ua/ho/godex/codewars/test3/RemovedNumbers.java
@@ -0,0 +1,10 @@
+package ua.ho.godex.codewars.test3;
+
+import java.util.List;
+
+public class RemovedNumbers {
+    public static List<long[]> removNb(long n) {
+        // your code
+        return null;
+    }
+}
