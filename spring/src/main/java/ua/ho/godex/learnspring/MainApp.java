300661fd570b29e915029f7a58b6ea0cd3ec9585
PablenkoB <pavlenkobohdan@gmail.com>
2018-05-10 17:30:52 +0300

    add test locale

--- spring/src/main/java/ua/ho/godex/learnspring/MainApp.java
+++ spring/src/main/java/ua/ho/godex/learnspring/MainApp.java
@@ -1,13 +1,33 @@
 package ua.ho.godex.learnspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import java.util.Locale;
 
 @SpringBootApplication
-public class MainApp {
+@PropertySource("classpath:application.yml")
+public class MainApp implements ApplicationRunner {
+
+    @Autowired
+    private ApplicationContext context;
+
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         SpringApplication.run(MainApp.class, args);
+
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Environment env = context.getEnvironment();
+        String key = "spring.profiles.active";
+        boolean containsFoo = env.containsProperty(key);
+        System.out.println(context.getMessage("greeting", null, Locale.FRANCE));
     }
 }
---
+++ spring/src/main/java/ua/ho/godex/learnspring/config/TypicalConfig.java
@@ -0,0 +1,18 @@
+package ua.ho.godex.learnspring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class TypicalConfig {
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.FRANCE);
+        return slr;
+    }
+}
