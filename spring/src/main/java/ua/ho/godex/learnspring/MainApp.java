73d4eb890e1b852b7c1f47341d9438f1d7714795
PablenkoB <pavlenkobohdan@gmail.com>
2018-05-10 16:57:42 +0300

    save learning spring core

--- spring/src/main/java/ua/ho/godex/learnspring/MainApp.java
+++ spring/src/main/java/ua/ho/godex/learnspring/MainApp.java
@@ -2,10 +2,12 @@ package ua.ho.godex.learnspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class MainApp {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         SpringApplication.run(MainApp.class, args);
     }
 }
