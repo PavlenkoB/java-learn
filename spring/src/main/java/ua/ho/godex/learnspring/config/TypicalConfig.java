233ef3df73bf5c26cf0973da2c86ce48217685b1
PablenkoB <pavlenkobohdan@gmail.com>
2018-05-16 11:32:32 +0300

    add resources handler

--- spring/src/main/java/ua/ho/godex/learnspring/config/TypicalConfig.java
+++
@@ -1,8 +0,0 @@
-package ua.ho.godex.learnspring.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-public class TypicalConfig {
-}
---
+++ spring/src/main/java/ua/ho/godex/learnspring/config/WebConfig.java
@@ -0,0 +1,20 @@
+package ua.ho.godex.learnspring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/public/**")
+                .addResourceLocations("classpath:/public/")
+                .setCacheControl(CacheControl.maxAge(1, TimeUnit.HOURS).cachePublic());
+    }
+}
