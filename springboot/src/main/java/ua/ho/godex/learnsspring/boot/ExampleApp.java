13b436ec4c0bcb4b6ef036d110ad076ad413c5e6
PablenkoB <pavlenkobohdan@gmail.com>
2018-05-17 17:53:24 +0300

    add my own config

--- springboot/src/main/java/ua/ho/godex/learnsspring/boot/ExampleApp.java
+++ springboot/src/main/java/ua/ho/godex/learnsspring/boot/ExampleApp.java
@@ -1,17 +1,21 @@
 package ua.ho.godex.learnsspring.boot;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.ho.godex.learnsspring.boot.config.AnotherComponent;
 
 @SpringBootApplication
 //@PropertySource("classpath:application.yml")
 //@EnableAutoConfiguration
 @RestController
-public class ExampleApp implements ApplicationRunner {
+public class ExampleApp implements CommandLineRunner {
+
+    @Autowired
+    AnotherComponent anotherComponent;
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleApp.class, args);
@@ -19,10 +23,11 @@ public class ExampleApp implements ApplicationRunner {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World!1222";
     }
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(String... args) throws Exception {
+        System.out.println("anotherComponent..." + anotherComponent.name);
     }
 }
---
+++ springboot/src/main/java/ua/ho/godex/learnsspring/boot/config/AnotherComponent.java
@@ -0,0 +1,20 @@
+package ua.ho.godex.learnsspring.boot.config;
+
+public class AnotherComponent {
+    public String name;
+
+    public AnotherComponent() {
+    }
+
+    public AnotherComponent(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
