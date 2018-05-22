bce1a192259fa98a754289c6743dd6d83fb1e7eb
PablenkoB <pavlenkobohdan@gmail.com>
2018-05-22 11:57:47 +0300

    save state

--- spring/src/main/java/ua/ho/godex/learnspring/config/SecurityConfig.java
+++ spring/src/main/java/ua/ho/godex/learnspring/config/SecurityConfig.java
@@ -7,7 +7,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.requestMatcher(EndpointRequest.toAnyEndpoint())
-                .authorizeRequests()
+                .authorizeRequests(
                 .anyRequest().permitAll();
     }
 }*/
