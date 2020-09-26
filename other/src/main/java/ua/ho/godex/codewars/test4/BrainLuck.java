80ca91abb2ff64c6fa5a6ef0e7e38574fe533825
godex <godexxxx@gmail.com>
2020-09-26 10:48:33 +0300

    init kata https://www.codewars.com/kata/526156943dfe7ce06200063e/train/java

---
+++ other/src/main/java/ua/ho/godex/codewars/test4/BrainLuck.java
@@ -0,0 +1,13 @@
+package ua.ho.godex.codewars.test4;
+
+import java.util.*;
+
+public class BrainLuck {
+
+    public BrainLuck(String code) {
+    }
+
+    public String process(String input) {
+        return "";
+    }
+}
